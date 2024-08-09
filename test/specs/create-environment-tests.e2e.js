ffc942a9865fff8aef724d0a030d52d491ef75a4
Ben Chidgey <ben@feedmypixel.com>
2024-08-09 16:48:54 +0100

    Updates to specs
    - Add logged in and logged out functionality
    - Add initial service tests more to come

--- test/specs/create-environment-tests.e2e.js
+++ test/specs/create-environment-tests.e2e.js
@@ -1,105 +1,114 @@
 import { browser, expect } from '@wdio/globals'
 
-import HomePage from 'page-objects/home.page'
 import CreatePage from 'page-objects/create.page'
+import ErrorPage from 'page-objects/error.page'
 import FormComponent from 'components/form.component'
+import HeadingComponent from 'components/heading.component'
 import TestSuitesPage from 'page-objects/test-suites.page'
 
 describe('Create environment tests', () => {
-  const testRepositoryName = `env-test-suite-${new Date().getTime()}`
+  describe('When logged out', () => {
+    before(async () => {
+      await CreatePage.open()
+    })
 
-  it('Login', async () => {
-    await HomePage.open()
-
-    await expect(HomePage.loginLink).toHaveText('Sign in')
-
-    await HomePage.loginLink.click()
-
-    await expect(HomePage.userName).toHaveText('admin')
-    await expect(HomePage.loginLink).toHaveText('Sign out')
-  })
-
-  it('Should be on the "Create" page', async () => {
-    await CreatePage.open()
-
-    await expect(browser).toHaveTitle(
-      'Create | Core Delivery Platform - Portal'
-    )
-    await expect(await CreatePage.navIsActive()).toBe(true)
-    await expect(CreatePage.appHeadingTitle('Create')).toExist()
-  })
-
-  it('Should be able to choose environment tests', async () => {
-    await expect(
-      FormComponent.legend('What would you like to create?')
-    ).toExist()
-
-    await FormComponent.inputLabel('Environment Test Suite').click()
-    await FormComponent.submitButton('Next').click()
-  })
-
-  it('Should be able to enter environment test details', async () => {
-    await expect(browser).toHaveTitle(
-      'Create environment test suite | Core Delivery Platform - Portal'
-    )
-    await expect(await CreatePage.navIsActive()).toBe(true)
-    await expect(
-      CreatePage.appHeadingTitle('Create environment test suite')
-    ).toExist()
-
-    await FormComponent.inputLabel('Name').click()
-    await browser.keys(testRepositoryName)
-
-    await FormComponent.inputLabel('Owning Team').click()
-    await browser.keys('Platform')
-
-    await FormComponent.submitButton('Next').click()
-  })
-
-  it('Should be able to view environment test summary', async () => {
-    await expect(browser).toHaveTitle(
-      'Summary environment test suite | Core Delivery Platform - Portal'
-    )
-    await expect(
-      CreatePage.appHeadingTitle('Summary environment test suite')
-    ).toExist()
-    await expect(
-      CreatePage.appHeadingCaption(
-        'Information about the new environment test suite you are going to create.'
+    it('Should show a "401" page', async () => {
+      await expect(browser).toHaveTitle(
+        'Unauthorized | Core Delivery Platform - Portal'
       )
-    ).toExist()
-
-    await FormComponent.submitButton('Create').click()
+      await expect(ErrorPage.title('401')).toExist()
+      await expect(ErrorPage.message()).toHaveText('Unauthorized')
+    })
   })
 
-  it('Should be redirected to create environment test suite status page', async () => {
-    await expect(browser).toHaveTitle(
-      `Creating ${testRepositoryName} test suite | Core Delivery Platform - Portal`
-    )
-    await expect(await TestSuitesPage.navIsActive()).toBe(true)
-    await expect(TestSuitesPage.appHeadingTitle(testRepositoryName)).toExist()
-    await expect(
-      TestSuitesPage.appHeadingCaption(
-        `Creating the ${testRepositoryName} test suite.`
+  describe('When logged in', () => {
+    const testRepositoryName = `env-test-suite-${new Date().getTime()}`
+
+    before(async () => {
+      await CreatePage.login()
+      await CreatePage.open()
+    })
+
+    it('Should be on the "Create" page', async () => {
+      await expect(browser).toHaveTitle(
+        'Create | Core Delivery Platform - Portal'
       )
-    ).toExist()
-    await expect(TestSuitesPage.overallProgress()).toHaveText('In-progress')
-  })
+      await expect(await CreatePage.navIsActive()).toBe(true)
+      await expect(HeadingComponent.title('Create')).toExist()
+    })
 
-  it('Should be redirected to "success" create environment test suite page', async () => {
-    await expect(browser).toHaveTitle(
-      `Created ${testRepositoryName} test suite | Core Delivery Platform - Portal`
-    )
-    await expect(await TestSuitesPage.navIsActive()).toBe(true)
-    await expect(TestSuitesPage.appHeadingTitle(testRepositoryName)).toExist()
-    await expect(
-      TestSuitesPage.appHeadingCaption(
-        `Created the ${testRepositoryName} test suite.`
+    it('Should be able to choose environment tests', async () => {
+      await expect(
+        FormComponent.legend('What would you like to create?')
+      ).toExist()
+
+      await FormComponent.inputLabel('Environment Test Suite').click()
+      await FormComponent.submitButton('Next').click()
+    })
+
+    it('Should be able to enter environment test details', async () => {
+      await expect(browser).toHaveTitle(
+        'Create environment test suite | Core Delivery Platform - Portal'
       )
-    ).toExist()
+      await expect(await CreatePage.navIsActive()).toBe(true)
+      await expect(
+        HeadingComponent.title('Create environment test suite')
+      ).toExist()
 
-    await TestSuitesPage.link('new environment test suite page').click()
+      await FormComponent.inputLabel('Name').click()
+      await browser.keys(testRepositoryName)
+
+      await FormComponent.inputLabel('Owning Team').click()
+      await browser.keys('Platform')
+
+      await FormComponent.submitButton('Next').click()
+    })
+
+    it('Should be able to view environment test summary', async () => {
+      await expect(browser).toHaveTitle(
+        'Summary environment test suite | Core Delivery Platform - Portal'
+      )
+      await expect(
+        HeadingComponent.title('Summary environment test suite')
+      ).toExist()
+      await expect(
+        HeadingComponent.caption(
+          'Information about the new environment test suite you are going to create.'
+        )
+      ).toExist()
+
+      await FormComponent.submitButton('Create').click()
+    })
+
+    it('Should be redirected to create environment test suite status page', async () => {
+      await expect(browser).toHaveTitle(
+        `Creating ${testRepositoryName} test suite | Core Delivery Platform - Portal`
+      )
+      await expect(await TestSuitesPage.navIsActive()).toBe(true)
+      await expect(HeadingComponent.title(testRepositoryName)).toExist()
+      await expect(
+        HeadingComponent.caption(
+          `Creating the ${testRepositoryName} test suite.`
+        )
+      ).toExist()
+      await expect(TestSuitesPage.overallProgress()).toHaveText('In-progress')
+    })
+
+    it('Should be redirected to "success" create environment test suite page', async () => {
+      await expect(browser).toHaveTitle(
+        `Created ${testRepositoryName} test suite | Core Delivery Platform - Portal`
+      )
+      await expect(await TestSuitesPage.navIsActive()).toBe(true)
+      await expect(HeadingComponent.title(testRepositoryName)).toExist()
+      await expect(
+        HeadingComponent.caption(
+          `Created the ${testRepositoryName} test suite.`
+        )
+      ).toExist()
+
+      await TestSuitesPage.link('new environment test suite page').click()
+    })
+
+    // TODO - add /test-suites/${testRepositoryName} test once stubs have been updated
   })
-
-  // TODO - add /test-suites/${testRepositoryName} test once stubs have been updated
 })
--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -2,26 +2,26 @@ import { expect } from '@wdio/globals'
 
 import HomePage from 'page-objects/home.page'
 
-describe('Login', () => {
-  beforeEach(async () => {
+describe('Log in and log out', () => {
+  before(async () => {
     await HomePage.open()
   })
 
   it('Should be able to sign in as "Admin" user', async () => {
-    await expect(HomePage.loginLink).toHaveText('Sign in')
+    await expect(HomePage.logInLink()).toHaveText('Sign in')
 
-    await HomePage.loginLink.click()
+    await HomePage.logInLink().click()
 
-    await expect(HomePage.userName).toHaveText('admin')
-    await expect(HomePage.loginLink).toHaveText('Sign out')
+    await expect(HomePage.userName()).toHaveText('admin')
+    await expect(HomePage.logOutLink()).toHaveText('Sign out')
   })
 
   it('Should be able to sign out', async () => {
-    await expect(HomePage.loginLink).toHaveText('Sign out')
+    await expect(HomePage.logOutLink()).toHaveText('Sign out')
 
-    await HomePage.loginLink.click()
+    await HomePage.logOutLink().click()
 
-    await expect(HomePage.userName).not.toExist()
-    await expect(HomePage.loginLink).toHaveText('Sign in')
+    await expect(HomePage.userName()).not.toExist()
+    await expect(HomePage.logInLink()).toHaveText('Sign in')
   })
 })
